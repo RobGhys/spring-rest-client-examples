5c6ed0f3fa3189fbd58eeace00e68a45e48d0dd4
robghys <Nalinnes1332!>
2021-07-20 08:41:22 +0200

    Made the program reactive

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -4,9 +4,13 @@ import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -28,5 +32,21 @@ public class ApiServiceImpl implements ApiService {
         UserData userData = restTemplate.getForObject(uriComponentsBuilder.toUriString(), UserData.class);
 
         return userData.getData();
+    }
+
+    // Mono, as the method will emit at most 1 item
+    @Override
+    public Flux<User> getUsers(Mono<Integer> limit) {
+        // Add query param of "limit" to the uriBuilder
+        // Exchange = flow of exchange between client and webserver
+        // Need to map the response to a Mono (and in specific, the UserData class)
+        return WebClient
+                .create(api_url)
+                .get()
+                .uri(uriBuilder -> uriBuilder.queryParam("limit", limit.block()).build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .flatMap(resp -> resp.bodyToMono(UserData.class))
+                .flatMapIterable(UserData::getData); // Returns a List that we convert to a WebClient
     }
 }
